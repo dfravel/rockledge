5d3ad30d700eaa001e026b92b418f29bedb01363
Dave Fravel <dave@bluegear.io>
2021-11-08 17:52:22 -0500

    added favicon

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
   <div class="flex h-screen font-sans">
     <div class="m-auto">
       <div class="rounded-md bg-black p-20 text-center">
-        <h2 class="text-3xl text-white uppercase mb-4 font-light">Coming Soon</h2>
+        <h2 class="text-3xl text-white uppercase mb-8 font-light">Coming Soon</h2>
         <img class="text-center mx-auto" alt="Rock Ledge Financial Group logo" src="./assets/logo_web_5.png" />
 
         <div class="mt-10 text-white text-center text-2xl font-light">49 Putnam Blvd #1010 Glastonbury, CT 06033</div>
