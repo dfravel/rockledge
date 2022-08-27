08940b62f17f9e441083fae80b931951348dcd4f
Dave Fravel <dave@bluegear.io>
2022-08-27 11:29:49 -0400

    removed coming soon

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h2 class="text-3xl text-white uppercase mb-8 font-light">Coming Soon</h2>
   <img class="text-center mx-auto" alt="Rock Ledge Financial Group logo" src="../assets/logo_web_5.png" />
 
   <div class="mt-10 text-white text-center text-2xl font-light">49 Putnam Blvd #1010 Glastonbury, CT 06033</div>
